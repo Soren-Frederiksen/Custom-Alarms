dd3f200949c90d878a3fa5e6e6b1f225f5a66145
Soren-Frederiksen <soren.frederiksen@logrhythm.com>
2022-11-10 08:21:55 -0700

    Update Get-LR-Cases-Report.ps1
    Fixed up some of the error checking

--- Get-LR-Cases-Report.ps1
+++ Get-LR-Cases-Report.ps1
@@ -64,8 +64,8 @@ if ($currentDate -eq "" )
 # Specify Output File in the form  Cases-<date>.txt
 $outputFile = "$outputPath" + "Cases-" + "$currentDate" + ".txt"
 
-# Now set $alarmDate to be one less than current day.  
-#    Will use this date to run Get-LRAlarms.  This is because the Alarms are returned in UTC time and so you want to make sure to include all days
+# Now set $caseDate to be one less than current day.  
+#    Will use this date to run Get-LRCases.  This is because the Alarms are returned in UTC time and so you want to make sure to include all days
 
 $caseDate = (Get-Date $currentDate).AddDays(-1)
 
@@ -105,59 +105,28 @@ $endDate = Get-Date $enddate -Format ("yyyy-MM-ddThh:mm:ss")
 #$allAlarms = Get-LrAlarms -ResultsOnly -DateInserted $alarmDate| Where-Object dateInserted -ge $startDate | Where-Object dateInserted -le $endDate | sort-object alarmId -unique
 
 $allCases = Get-LrCases
-$todaysCases = Get-LrCases -CreatedAfter $startDate -CreatedBefore $startDate.AddDays(1)
-$closedCases = $todaysCases | where-object dateclosed -CNotlike ""
-$allClosedCases = $allCases | where-object dateclosed -CNotlike ""
-$casesOpen = $allCases.count - $allClosedCases.count
-
-<#
-if ($allAlarms.Error -eq $true) {
-    return $allAlarms
+#
+if ($allCases.Error -eq $true) 
+{
+    return $allCases
 } else 
 {
-    $allAlarms = $allAlarms | Where-Object dateInserted -ge $startDate | Where-Object dateInserted -le $endDate | sort-object alarmId -unique
+	$todaysCases = Get-LrCases -CreatedAfter $startDate -CreatedBefore $startDate.AddDays(1)
+	if ($todaysCases.Error -eq $true) 
+	{
+ 	   return $todaysCases
+	}
+	$closedCases = $todaysCases | where-object dateclosed -CNotlike ""
+	$allClosedCases = $allCases | where-object dateclosed -CNotlike ""
+	$casesOpen = $allCases.count - $allClosedCases.count
 }
 
-# Do not include any alarms in cluded in the IgnoreAlarms  array
+<#
+# Do not include any alarms included in the IgnoreAlarms  array
 foreach ($ignore in $ignorealarm)
 {
     $allAlarms = $allAlarms | Where-Object alarmRuleName -CNotlike "*$ignore*"
 }
-#>
-
-<# Now collect alarms of each status types (types 1-9)
-AlarmStatus		AlarmStatusName
-0				New
-1				Open: Open
-2				Open: Working
-3				Open: Escalated
-4				Closed: Closed
-5				Closed: False Alarm
-6				Closed: Monitor
-7				Closed: Reported
-8				Closed: Resolved
-9				Closed: Unresolved
-#>
-
-<#
-$newAlarms = $allAlarms | Where-Object -Property alarmStatus -eq 0 
-$openedAlarms = $allAlarms | Where-Object -Property alarmStatus -eq 1
-$workingAlarms = $allAlarms | Where-Object -Property alarmStatus -eq 2
-$escalatedAlarms = $allAlarms | Where-Object -Property alarmStatus -eq 3
-$closedAlarms = $allAlarms | Where-Object -Property alarmStatus -eq 4
-$falseAlarms = $allAlarms | Where-Object -Property alarmStatus -eq 5
-$monitorAlarms = $allAlarms | Where-Object -Property alarmStatus -eq 6
-$reportedAlarms = $allAlarms | Where-Object -Property alarmStatus -eq 7
-$resolvedAlarms = $allAlarms | Where-Object -Property alarmStatus -eq 8
-$unresolvedAlarms = $allAlarms | Where-Object -Property alarmStatus -eq 9
-
-# Now get all those alarms tht have been assigned to a case
-$assignedAlarms = $allAlarms | Where -Property associatedCases -ne ""
-
-# Now sum up all the alarm types
-$totalOpenedAlarms = @($newAlarms).count + @($openedAlarms).count + @($workingAlarms).count + @($escalatedAlarms).count
-$totalClosedAlarms = @($closedAlarms).count + @($falseAlarms).count + @($monitorAlarms).count + @($reportedAlarms).count + @($resolvedAlarms).count + @($unresolvedAlarms).count
-$unassignedAlarms = @($allAlarms).count - @($assignedAlarms).count
 #>
 
 # Output a summary of all alarm data counts.
